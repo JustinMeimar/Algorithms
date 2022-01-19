8e22286f730eedecd89f13408b55b38366048c86
Justin Meimar <j.meimar111@gmail.com>
2022-01-19 11:28:16 -0700

    no array

--- 3n2.c
+++ 3n2.c
@@ -6,42 +6,41 @@ void minMax(int *a, int n ){
 	//works for even lengthed arrays..	
 	//length of sub arrays
 	int i = 0, j=0;
-	
-	int small[SIZE/2-1] = {0};
-	int large[SIZE/2-1] = {0};			 
-	
-	int min = a[0] > a[1] ? a[1] : a[0]; //needs MAX INT
-	int max = a[1] > a[0] ? a[1] : a[0]; //not the move
-	
-	for(i=2; i<(n - n%2); i+=2){	
+	int min, max;
+	if(a[0] > a[1]){			// +1 key comparison
+		min = a[1];
+		max = a[0];
+	}else{
+		min = a[0];
+		max = a[1];
+	}
+	for(i=2; i<(n-n%2); i+=2){ //                 (n/2)	
 		//printf("%d, %d\n", a[i], a[i+1]);
-		if(a[i] < a[i+1]){
-			small[j] = a[i];
-			large[j] = a[i+1];
+		if(a[i] < a[i+1]){						// + (n/2) -1 key comparison
+			if(min > a[i]){
+				min = a[i];
+			}
+			if(max < a[i+1]){
+				max = a[i+1];
+			}
 		}else{
-			small[j] = a[i+1];
-			large[j] = a[i];
-		}
-		if(small[j] < min){
-			min = small[j];
-		}
-		if(large[j] > max){
-			max = large[j];
+			if(min > a[i+1]){
+				min = a[i+1];
+			}
+			if(max < a[i]){
+				max = a[i];
+			}
 		}
 		j++;
 	}
-	//handle odd element at end
 	if(n%2 == 1){
-		if(a[n-1] > max){
+		if(a[n-1] > max){						// +1 key comp
 			max = a[n-1];
 		}
-		if(a[n-1] < min){
+		if(a[n-1] < min){						// +1 key comp
 			min = a[n-1];
 		}
 	}
-	for(int i  =0; i<(SIZE/2)-1; i++){printf("%d,", small[i]);}	
-	printf("\n");
-	for(int i  =0; i<(SIZE/2)-1; i++){printf("%d,", large[i]);}
 	printf("\nLargest: %d, Smallest: %d", max, min);
 	//total comparison 3n +1 need 3n/2		
 }
