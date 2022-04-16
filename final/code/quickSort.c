dea368597d809822ae22e0b17a612b1fd4ec8696
Justin Meimar <j.meimar111@gmail.com>
2022-04-16 10:33:45 -0600

    new partition method

--- final/code/quickSort.c
+++ final/code/quickSort.c
@@ -22,29 +22,31 @@ void printArr(int arr[10], int n){
 	printf("\n");
 }
 
-int partition(int arr[10], int l, int r){
-	int i = l;
-	int j = r-1;
-	int piv = r;
-
-	printf("%d, %d\n", l, r);
+int partition(int arr[10], int p, int r){
+	int piv = arr[r];
+	int i = -1;
+	for(int j = i+1; j < r; j++){
+		if(arr[j] <= piv){
+			i++;
+			int temp = arr[j];
+			arr[j] = arr[i];
+			arr[i] = temp;
+		}
+	}
+	int temp = arr[i+1];
+	arr[i+1] = arr[r];
+	arr[r] = temp;
 	
-	
-	
-	return j;
+	return i+1;
 }
 
-void quickSort(int arr[10], int l, int r){
-	if(l >= r){
-		printArr(arr, 10);	
-		return;
+void quickSort(int arr[10], int p, int r){
+	if(p < r){
+		int q = partition(arr, p, r);
+		quickSort(arr, p, q-1);
+		quickSort(arr, q+1, r);
+
 	}
-
-	int p = partition(arr, l, r);
-
-	quickSort(arr, l, p-1);
-	quickSort(arr, p+1, r);
-
 }
 
 int main(){
